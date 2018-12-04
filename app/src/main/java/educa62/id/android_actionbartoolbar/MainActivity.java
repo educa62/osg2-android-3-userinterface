1d0a29f47562a919a40ffeefb753b668a27253c4
teddy <teddy.pangkat@gmail.com>
2018-12-04 14:09:28 +0700

    init

---
+++ app/src/main/java/educa62/id/android_actionbartoolbar/MainActivity.java
@@ -0,0 +1,39 @@
+package educa62.id.android_actionbartoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.TextView;
+;import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextView mTextTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.myToolbar);
+        mTextTime = (TextView) findViewById(R.id.txtTime);
+        setSupportActionBar(mToolbar);
+
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String formatDate = df.format(c.getTime());
+
+        mTextTime.setText(formatDate);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+}
